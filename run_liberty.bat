a236c5fa31bdcb62c4bd71e8585759066871bc38
Slawomir Luty <slawomir.luty@gmail.com>
2020-11-09 23:02:44 +0100

    Liberty-managed

--- run_liberty.bat
+++ run_liberty.bat
@@ -1,4 +1,5 @@
 call mvn -Pliberty-managed clean package 
-call mvn -Pliberty-managed liberty:create liberty:install-feature liberty:deploy 
+call mvn -Pliberty-managed liberty:create liberty:install-feature
+rem liberty:deploy 
 call mvn -Pliberty-managed liberty:configure-arquillian 
-call mvn -Pliberty-managed failsafe:integration-test 
+rem call mvn -Pliberty-managed failsafe:integration-test 
